f90c227418045773e6fa69679a88f20e2ffd7e49
die-h <diegohermida94@gmail.com>
2022-01-08 04:47:49 -0300

    The game is now working

--- script.js
+++ script.js
@@ -27,28 +27,39 @@ function playerPlay() {
 
 // This function will play a round of the game
 
-function playRound(playerSelection, computerSelection) {
+function playRound(ps, cs) {
 
-    p = playerSelection;
-    c = computerSelection;
-
-    console.log(p, c);
-    return;
+    if (ps === cs) {
+        return 'It\'s a tie!';
+    }
+    else if (ps  === "rock" && cs === "scissor" || ps === "paper" && cs === "rock"
+    || ps === "scissor" && cs === "paper"){
+        return `You Win! ${ps} wins over ${cs}`
+    }
+    else{
+        return `You Lose! ${cs} wins over ${ps}` 
+    }
+    
 }
 
 // This function plays a game of 5 rounds
 
 function game() {
 
-    for (let i = 0; i < 5; i++) {
+    let table = [];
 
-        let playerSelection = playerPlay();
-        let computerSelection = computerPlay();
+        for (i = 0; i < 5; i++) {
 
-        playRound(playerSelection, computerSelection)
-    }
+        let ps = playerPlay().toString();
+        let cs = computerPlay().toString();
+        
 
-    return;
+        table[i] = playRound(ps, cs);
+
+        console.log(table[i]);
+        }
+        
+        return;
 }
 
 
